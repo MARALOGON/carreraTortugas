e16111f28e113dd4b5c0d0b823eb76ab9fdb6b5d
Marcos Alonso <marcosalonso@MacBook-Pro-de-Marcos.local>
2021-04-18 08:29:26 +0200

    Version definitiva carrera Tortugas con odulos turtle y random

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 class Circuito():
     corredores = []
@@ -11,11 +12,11 @@ class Circuito():
         self.__screen.setup(width, height) 
         self.__screen.bgcolor('lightgrey')
         self.__startLine = -width/2 + 20
-        self.__finishLine = -width/2 - 20
+        self.__finishLine = width/2 - 20
         
         self.__createRunners()
         
-    def ___createRunners(self):   
+    def __createRunners(self):   
         for i in range(4): #Este bucle lo que hace es crear a los 4 corredorres
             new_turtle = turtle.Turtle()
             new_turtle.color(self.__colorTurtle[i])
@@ -25,8 +26,22 @@ class Circuito():
             
             
             self.corredores.append(new_turtle)
-            
+     
+    def competir(self):
+         
+         hayGanador = False
+         
+         while not hayGanador:
+             for tortuga in self.corredores:
+                 avance = random.randint(1, 6) #con esta variable se simula un lanzamiento de dados utilizando el modulo random
+                 tortuga.forward(avance)
+                                 
+                 if tortuga.position()[0] >= self.__finishLine:
+                     hayGanador = True
+                     print("La tortuga de color {} ha ganado".format(tortuga.color()[0]))
+                     break
+
 
 if __name__ == '__main__':
     circuito = Circuito(640, 480)
-        
+    circuito.competir()    
